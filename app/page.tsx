9dda781c52a46235275d8e854a761c402ed3e96d
Chihab Derghal <chihabderghal1@gmail.com>
2024-02-29 21:10:21 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import Hero from "@/components/Hero";
 import Club from "@/components/Club";
+import Newbie from "@/components/Newbie";
 
 export default function Home() {
     return (
         <div>
             <Hero/>
             <Club/>
+            <Newbie/>
         </div>
     );
 }
