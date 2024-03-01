9236be8253e2f95d9f2146889b3e82a7b0499557
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-02 00:39:23 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 function Hero() {
     return (
-        <section className="grid gap-8 md:grid-cols-2 bg-[#0d0d0d] text-white py-72 px-28">
+        <section className="grid gap-8 md:grid-cols-2 bg-[#0d0d0d] text-white pt-32 px-28">
             {/* LEFT SIDE */}
             <div className="flex flex-col justify-center items-center text-center uppercase py-20">
                 <div className="text-4xl font-bold pb-6">
@@ -20,7 +20,7 @@ function Hero() {
             </div>
             {/* RIGHT SIDE */}
             <div className="flex flex-col justify-center items-center relative py-20">
-                <Image src="/nextech.png" alt="event" width={250} height={250} className="flex justify-center"/>
+                <Image src="/nexverse.png" alt="event" width={280} height={280} className="flex justify-center"/>
             </div>
         </section>
     )
