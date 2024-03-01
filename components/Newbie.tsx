e32f16814983edbeed4c277d1a34ce4339cad4d2
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-01 11:55:16 +0100

    Update Newbie.tsx

--- components/Newbie.tsx
+++ components/Newbie.tsx
@@ -5,15 +5,15 @@ import React from "react";
 function Newbie() {
     return(
         <div className="py-28">
-            <div className="bg-[#ED1C24] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 py-28 px-36">
-                <div className="flex text-center uppercase text-white text-4xl py-10 lg:justify-around">
+            <div className="bg-[#ED1C24] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 py-4 px-44 lg: justify-center">
+                <div className="flex text-center uppercase text-white text-4xl py-10 lg:justify-start">
                     jois us, and become an active memeber in our future events
                 </div>
 
-               <div className="flex items-center content-center justify-center px-28">
+               <div className="flex items-center content-center justify-center px-20">
                    <Link href="#"
-                         className="uppercase border-red-600 text-white text-center rounded-2xl p-2 bg-[#0d0d0d] content-center lg:flex">
-                       <div className="flex justify-center items-center py-1 pt-2 uppercase px-4">
+                         className="uppercase text-white text-center rounded-2xl p-2 bg-[#0d0d0d] lg:flex">
+                       <div className="flex justify-center items-center py-1 pt-2 uppercase px-4 font-bold">
                            fill the form
                        </div>
                    </Link>
