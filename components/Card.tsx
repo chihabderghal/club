6ef795f3319c8c48a813afdb8548543a2fb1e36a
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-02 00:38:11 +0100

    Add Card component

---
+++ components/Card.tsx
@@ -0,0 +1,16 @@
+function Card() {
+    return(
+        <div className="border border-white my-6 lg:mx-6 text-center content-center text-white rounded-xl hover:bg-[#676767] hover:scale-110 transition hover:duration-200">
+            <div className="uppercase text-4xl font-bold py-5">
+                This is the question?
+            </div>
+
+            <div className="uppercase text-xl font-light py-5">
+               this is the answer of the following qestion, so read it clearly.Aot
+                is the best anime in the world ,then solo leveling
+            </div>
+        </div>
+    )
+}
+
+export default Card;
