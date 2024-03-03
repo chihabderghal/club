5dd8f0335d7b689968f9eaf17b589a673509015a
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-03 21:15:33 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,8 @@ export default function Home() {
             <div className="mb-20">
                 <h1 className="uppercase text-center text-5xl pt-3 text-white font-bold">faqs</h1>
 
-                <div className="mt-10 mx-10 px-10 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-[50px]">
+                <div
+                    className="mt-10 mx-10 px-10 lg:mx-22 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-[50px]">
                     <Card/>
                     <Card/>
                     <Card/>
@@ -44,6 +45,62 @@ export default function Home() {
             </div>
 
             <div className="border border-white hidden my-6 mx-[400px] lg:flex md:hidden"/>
+
+            <div>
+                <h1 className="uppercase text-center text-5xl pt-3 text-white font-bold">Contact us</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+
+                    <div className="my-20 mx-3 md:px-10 lg:px-10">
+                        <form>
+                            <div className="flex flex-col justify-center text-white py-3">
+                                <label className="font-bold px-4 py-1">Name:</label>
+                                <input type="text"
+                                       placeholder="John Doe"
+                                       className="font-light bg-[#0d0d0d] border border-white rounded-xl py-3 px-4"/>
+                            </div>
+
+                            <div className="flex flex-col justify-center text-white py-3">
+                                <label className="font-bold px-4 py-1">Email:</label>
+                                <input type="email"
+                                       placeholder="exemple123@gmail.com"
+                                       className="font-light bg-[#0d0d0d] border border-white rounded-xl py-3 px-4"/>
+                            </div>
+
+                            <div className="flex flex-col justify-center text-white py-3">
+                                <label className="font-bold px-4 py-1">Message:</label>
+                                <textarea cols={25} rows={10} placeholder="Write Your Message"
+                                          className="font-light bg-[#0d0d0d] border border-white rounded-xl py-4 px-4"></textarea>
+                            </div>
+
+                            <div className="flex justify-center my-5">
+                                <button
+                                    className="uppercase font-bold text-white text-center rounded-xl p-3 bg-[#0d0d0d] border-2 border-[#ED1C24] content-cente lg:flex hover:bg-[#ED1C24] hover:duration-500"
+                                >
+                                    Send Message
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div className="lg:flex lg:flex-col md:flex md:flex-col md:justify-center md:my-20 md:border-l-2  md:border-l-white lg:justify-center lg:my-20 lg:border-l-2 lg:border-l-white">
+                        <h1 className="text-white text text-center text-xl font-light px-5 lg:px-20">
+                            Feel free to reach us out via the form below. All you need to do is to send us an email or
+                            join
+                            us on site.
+                            You can also message us on one of our social media, we will make sure to answer your right
+                            away
+                            !
+                        </h1>
+                        <div className="l">
+                            <div className="flex flex-row items-center my-10 justify-center md:felx md:flex-col lg:flex-row">
+                                <Image src="/gmail.png" alt="gmail" width={45} height={45} className="mx-4"/>
+                                <h2 className="font-light text-white text-2xl text-center">nextchsquad@gmail.com</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 
         </div>
     );
