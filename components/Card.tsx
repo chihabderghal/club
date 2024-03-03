067d80701dcde435d89b143d4e988490535f97e3
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-03 21:14:45 +0100

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,11 +1,11 @@
 function Card() {
     return(
-        <div className="border border-white my-6 lg:mx-6 text-center content-center text-white rounded-xl hover:bg-[#676767] hover:scale-110 transition hover:duration-200">
-            <div className="uppercase text-4xl font-bold py-5">
-                This is the question?
+        <div className="border border-white my-6 lg:mx-6 text-center content-center text-white rounded-xl from-gray-800 to-black hover:scale-110 transition hover:duration-200">
+            <div className="uppercase text-4xl font-bold mx-4 py-5">
+                What is our goal in this year?
             </div>
 
-            <div className="uppercase text-xl font-light py-5">
+            <div className="uppercase text-xl font-light mx-4 py-5">
                this is the answer of the following qestion, so read it clearly.Aot
                 is the best anime in the world ,then solo leveling
             </div>
