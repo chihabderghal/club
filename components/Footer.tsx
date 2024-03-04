ceb672a2dec24e8e37d96f3261519b85d098f322
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-04 16:07:56 +0100

    Add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,75 @@
+import Link from "next/link";
+import Image from "next/image";
+import {COPY, EMAIL, FOOTER_LINKS, LOCATION, RIGHT, SOCIAL_MEDIA} from "@/data/constant";
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="text-white px-10">
+            <div className="grid grid-cols-1 lg:grid-cols-4 lg:gap-8">
+                <Link href="/" className="flex flex-col items-center py-2 my-10 md:mx-17">
+                    <Image src="/nextech.png" alt="nextech" width={58} height={30}/>
+                </Link>
+
+
+                {/* Location & Email */}
+                <div className="text-center  my-10 lg:text-start">
+                    <div className="pb-5 py-2 px-10 lg:px-0">
+                        {LOCATION}
+                    </div>
+                    <div className="text-xl text-center lg:text-start font-light">
+                        <strong>
+                            {EMAIL}
+                        </strong>
+                    </div>
+                </div>
+
+                {/* Learn More */}
+                <div className="grid grid-cols-1 my-10 place-items-center lg:pl-24 lg:place-items-start">
+                    <div className="font-bold text-xl py-2">
+                        Learn More:
+                    </div>
+                    {FOOTER_LINKS.map((urls) => (
+                        // eslint-disable-next-line react/jsx-key
+                        <div className="font-light py-1">
+                            <ul>
+                                <Link href={urls.link}>
+                                    <li>{urls.title}</li>
+                                </Link>
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+
+                {/* Social Media */}
+                <div className="grid grid-cols-1 my-10 place-items-center text-start lg:place-items-start">
+                    <div className="font-bold text-xl py-2">
+                        Social Media:
+                    </div>
+                    {SOCIAL_MEDIA.map((social: { name: string, link: string, image_path: string }) => (
+                        // eslint-disable-next-line react/jsx-key
+                        <div className="font-light">
+                            <ul>
+                                <Link href={social.link}>
+                                    <li className="flex flex-row items-center gap-2 py-2 lg:py-0">
+                                        <Image src={social.image_path} alt={social.name} width={20} height={20}/>
+                                        {social.name}
+                                    </li>
+                                </Link>
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+
+
+            </div>
+
+
+            <div className="text-white text-xs font-light text-center py-4">
+                {COPY} &copy; {RIGHT}
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
