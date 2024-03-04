ac2580e0c45bcc46977951d4126483f9a66ac6af
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-04 15:08:27 +0100

    Add constant.ts

---
+++ data/constant.ts
@@ -0,0 +1,47 @@
+export const FOOTER_LINKS = [
+    {
+        title: "Event",
+        link:  "/event"
+    },
+    {
+        title: "FAQs",
+        link:  "/faqs"
+    },
+    {
+        title: "Newbie",
+        link:  "/Newbie"
+    },
+    {
+        title: "Members",
+        link:  "/members"
+    },
+    {
+        title: "Contact Us",
+        link:  "/contact-us"
+    },
+    {
+        title: "About Us",
+        link:  "/about-us"
+    },
+]
+
+export const SOCIAL_MEDIA: {name: string, link: string}[] = [
+    {
+        name: "Facebook",
+        link: "https://www.facebook.com/people/NextTech/61553474487971/?mibextid=ZbWKwL",
+    },
+    {
+        name: "Instagram",
+        link: "https://www.instagram.com/nextech.squad/?igshid=OHFwZmNvb2QwcDVi",
+    },
+    {
+        name: "Linkdin",
+        link: "#", // NO link for now
+    },
+]
+
+export const LOCATION: string = "Université Constantine 2 Abdelhamid Mehri – Nouvelle ville Ali Mendjeli BP :" +
+                                " 67A, Constantine – Algérie La Nouvelle Ville Ali Mendjeli, 25016";
+export const EMAIL: string = "nextechsquad@gmail.com";
+export const COPY: string = "Copyright";
+export const RIGHT: string = "2024 - All rights reserved";
