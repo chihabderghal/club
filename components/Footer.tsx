877774b64bcc50fbfa8d61cbf50f16f72c198e12
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-06 16:19:02 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,11 +27,11 @@ function Footer() {
                 {/* Learn More */}
                 <div className="grid grid-cols-1 my-10 place-items-center lg:pl-24 lg:place-items-start">
                     <div className="font-bold py-2">
-                        More:
+                        Learn More:
                     </div>
                     {LINKS.map((urls) => (
                         // eslint-disable-next-line react/jsx-key
-                        <div className="font-light px-auto mx-auto py-1">
+                        <div className="font-light py-1">
                             <ul>
                                 <Link href={urls.link}>
                                     <li>{urls.title}</li>
@@ -44,14 +44,14 @@ function Footer() {
                 {/* Social Media */}
                 <div className="grid grid-cols-1 my-10 place-items-center text-start lg:place-items-start">
                     <div className="font-bold py-2">
-                        Media:
+                        Social Media:
                     </div>
                     {SOCIAL_MEDIA.map((social: { name: string, link: string, image_path: string }) => (
                         // eslint-disable-next-line react/jsx-key
                         <div className="font-light">
                             <ul>
                                 <Link href={social.link}>
-                                    <li className="flex flex-row items-center gap-2 px-auto mx-auto py-2 lg:py-0">
+                                    <li className="flex flex-row items-center gap-2 py-2 lg:py-0">
                                         <Image src={social.image_path} alt={social.name} width={20} height={20}/>
                                         {social.name}
                                     </li>
