e7e4f588d195f93ff4a55de48c97b909b5f9d812
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-06 23:43:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -102,10 +102,12 @@ export default function Home() {
                                 </h2>
                             </div>
 
-                            <div className="flex flex-col justify-center items-center gap-8 px-8">
+                            <div className="flex flex-col items-center relativ">
+                                <h1 className="uppercase text-center text-3xl py-3 text-white font-bold">location</h1>
+
                                 <iframe
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3217.729946319797!2d6.567431376277398!3d36.246053598739984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12f1655aaa0d63ab%3A0xa80cc72a9da3c77b!2sUniversity%20of%20Constantine%202%20-%20Abdelhamid%20Mehri!5e0!3m2!1sen!2sdz!4v1709762913511!5m2!1sen!2sdz"
-                                    width="400" height="350" loading="lazy" className="rounded-2xl"
+                                    width="320" height="320" loading="lazy" className="rounded-2xl"
                                     referrerPolicy="no-referrer-when-downgrade"></iframe>
                             </div>
                         </div>
