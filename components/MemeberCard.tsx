9b8f8aa5443f762dc7c755edafd83553fea67e72
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-08 00:34:23 +0100

    Update MemeberCard.tsx

--- components/MemeberCard.tsx
+++ components/MemeberCard.tsx
@@ -22,7 +22,8 @@ function MemeberCard(member: Member) {
         <div className="lg:mx-0 text-center content-end  text-white border-4 rounded-2xl">
             <div className="flex flex-col items-center">
                 <div className="flex justify-center mt-10 ">
-                    <Image src={member.image_path} alt="image" className="rounded-full border-2ddiU border-white" width={240} height={240}/>
+                    <Image src={member.image_path} alt="image" className="rounded-full border-2ddiU border-white"
+                           width={240} height={240}/>
                 </div>
                 <div className="py-5 flex flex-row justify-center items-center">
                     <h1 className="font-bold text-xl px-3 items-center justify-center text-center">{member.name}</h1>
@@ -35,27 +36,28 @@ function MemeberCard(member: Member) {
                         {member.team}
                     </div>
                     <div
-                        className="border-2 border-green-600 text-center text-green-600 relative p-2 rounded-3xl uppercase">
+                        className={`border-2  text-center  relative p-2 rounded-3xl uppercase ${member.position == "president" ? 'border-yellow-400 text-yellow-400' : 'text-green-600 border-green-600'}`}>
                         {member.position}
                     </div>
                 </div>
 
-                <div className="py-auto mx-5 mb-5 text-center flex justify-center items-start h-[220px] lg:h-[210px] md:h-[150px] font-light">
+                <div
+                    className="py-auto mx-5 mb-5 text-center flex justify-center items-start h-[220px] lg:h-[210px] md:h-[150px] font-light">
                     {member.descritpion}
                 </div>
             </div>
-                <div className="flex flex-row justify-center py-5 gap-10 ">
-                    <Link href={member.github} className="hover:scale-125 hover:duration-300">
-                        <FontAwesomeIcon icon={faGithub} width={30} height={30}/>
-                    </Link>
-                    <Link href={member.linkedin} className="hover:scale-125 hover:duration-300">
-                        <FontAwesomeIcon icon={faLinkedin} width={30} height={30}/>
-                    </Link>
-                    <Link href={member.instagram} className="hover:scale-125 hover:duration-300">
-                        <FontAwesomeIcon icon={faInstagram} width={30} height={30}/>
-                    </Link>
-                </div>
+            <div className="flex flex-row justify-center py-5 gap-10 ">
+                <Link href={member.github} className="hover:scale-125 hover:duration-300">
+                    <FontAwesomeIcon icon={faGithub} width={30} height={30}/>
+                </Link>
+                <Link href={member.linkedin} className="hover:scale-125 hover:duration-300">
+                    <FontAwesomeIcon icon={faLinkedin} width={30} height={30}/>
+                </Link>
+                <Link href={member.instagram} className="hover:scale-125 hover:duration-300">
+                    <FontAwesomeIcon icon={faInstagram} width={30} height={30}/>
+                </Link>
             </div>
+        </div>
     );
 }
 
