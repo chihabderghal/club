31c7ad871f177cc19a8f238dee6e8c8e65c3be20
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-07 21:52:41 +0100

    Add members.ts

---
+++ data/members.ts
@@ -0,0 +1,22 @@
+export const MEMEBRS = [
+    {
+        image_path: "/images/abdou_image.jpg",
+        name: "Abdou Ouchen",
+        team: "design",
+        position: "leader",
+        description: "A creative coder who effortlessly blends graphic design with technical expertise. With a love for both creating visually stunning designs and diving deep into lines of code, they find balance in embracing moments of relaxation and leisure.",
+        github: "https://github.com/abdoou-exe",
+        linkedin: "https://www.linkedin.com/in/ouchen-abdelillah-28ba092a4/",
+        instagram: "https://www.instagram.com/abdoou.exe?igsh=b2w4aDJnNDd0cG1r",
+    },
+    {
+        image_path: "/images/readouane_image.jpg",
+        name: "Redouane LEBAD",
+        team: "HR&L",
+        position: "member",
+        description: "Passionate and still-in-progress.\n",
+        github: "https://github.com/redouane731\n",
+        linkedin: "",
+        instagram: "https://instagram.com/redyeboi\n",
+    },
+]
