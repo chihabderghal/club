cc660a8b22d6ee5242f009e9981431f2b24d2363
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-07 07:39:35 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import {SOCIAL_MEDIA} from "@/data/constant";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function Hero() {
     return (
         <section className="grid gap-8 md:grid-cols-2 bg-[#0d0d0d] text-white pt-32 px-5 lg:px-28 md:px-28">
             {/* LEFT SIDE */}
             <div className="flex flex-col justify-center items-center text-center uppercase py-20">
-                <div className="text-4xl font-bold pb-6">
+                <div className="text-4xl font-bold pb-4 pt-10">
                     <h1>nextech</h1>
                     <h1>your hub for tech</h1>
                     <h1>exellence</h1>
@@ -17,10 +19,16 @@ function Hero() {
                       className="uppercase font-bold text-white text-center rounded-xl p-3 bg-[#0d0d0d] border-2 border-[#ED1C24] content-cente lg:flex hover:bg-[#ED1C24] hover:duration-500">
                     event
                 </Link>
+                <div className="flex flex-row my-4 gap-4 ">
+                    {SOCIAL_MEDIA.map((social) => (
+                        // eslint-disable-next-line react/jsx-key
+                        <FontAwesomeIcon icon={social.icon} width={25} height={25} className="hover:scale-125 hover:duration-300"/>
+                    ))}
+                </div>
             </div>
             {/* RIGHT SIDE */}
             <div className="flex flex-col justify-center items-center relative py-20">
-                <Image src="/nexverse.png" alt="event" width={280} height={280} className="flex justify-center"/>
+                <Image src="/nexverse.png" alt="event" width={270} height={270} className="flex justify-center"/>
             </div>
         </section>
     )
