53d6cfd3796684860cfddf5764936de7dff2631f
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-08 00:08:51 +0100

    Update members page

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -4,22 +4,28 @@ import MemeberCard from "@/components/MemeberCard";
 
 function members() {
     return (
-        <div className="mt-10 mx-10 lg:px-10 lg:mx-22 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-[50px]">
-            {
-                MEMEBRS.map((member) => (
-                    // eslint-disable-next-line react/jsx-key
-                    <MemeberCard
-                        image_path={member.image_path}
-                        name={member.name}
-                        team={member.team}
-                        position={member.position}
-                        descritpion={member.description}
-                        github={member.github}
-                        linkedin={member.linkedin}
-                        instagram={member.instagram}
-                    />
-                ))
-            }
+        <div>
+            <h1 className="uppercase text-center text-5xl pt-10 text-white font-bold">our members</h1>
+
+            <div className="mt-10 mx-10 lg:px-10 lg:mx-22 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-[50px]">
+
+                {
+                    MEMEBRS.map((member) => (
+                        // eslint-disable-next-line react/jsx-key
+                        <MemeberCard
+                            image_path={member.image_path}
+                            name={member.name}
+                            team={member.team}
+                            position={member.position}
+                            system={member.system}
+                            descritpion={member.description}
+                            github={member.github}
+                            linkedin={member.linkedin}
+                            instagram={member.instagram}
+                        />
+                    ))
+                }
+            </div>
         </div>
     );
 }
