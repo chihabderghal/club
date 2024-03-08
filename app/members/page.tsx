d202de13ce832f4d75858e6ac05371729cec5583
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-08 14:01:36 +0100

    Update Members page

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -4,7 +4,7 @@ import MemeberCard from "@/components/MemeberCard";
 
 function members() {
     return (
-        <div>
+        <div className="mb-20">
             <h1 className="uppercase text-center text-5xl pt-10 text-white font-bold">our members</h1>
 
             <div className="mt-10 mx-2 lg:px-10 lg:mx-22 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-[50px]">
