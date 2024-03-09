c765676968cabc213c91ce196cda7b70449d3382
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-09 21:27:24 +0100

    Add accordion.ts

---
+++ data/accordion.ts
@@ -0,0 +1,29 @@
+export const QUESTION_EVENT: { question: string, answer: string }[] = [
+    {
+        question: "Are there leadership opportunities IT club?",
+        answer: "Yes, absolutely! Our IT club offers various leadership opportunities for members who are interested in taking on roles such as event coordinators, workshop facilitators, project managers," +
+            " or executive board members. These positions provide valuable experience in leadership, teamwork, and project management.",
+    },
+    {
+        question: "How does your IT club engage with the broader tech community?",
+        answer: "Our IT club actively engages with the broader tech community through partnerships with local tech companies, participation in tech conferences and competitions, hosting guest speakers from the industry, and organizing networking events where members can connect with professionals in the field. yes"
+    },
+    {
+        question: "Are there leadership opportunities available within your IT club?",
+        answer: "Yes, absolutely! Our IT club offers various leadership opportunities for members who are interested in taking on roles such as event coordinators, workshop facilitators, project managers," +
+            " or executive board members. These positions provide valuable experience in leadership, teamwork, and project management.",
+    },
+    {
+        question: "How does your IT club engage with the broader tech community?",
+        answer: "Our IT club actively engages with the broader tech community through partnerships with local tech companies, participation in tech conferences and competitions, hosting guest speakers from the industry, and organizing networking events where members can connect with professionals in the field. yes"
+    },
+    {
+        question: "Are there leadership opportunities available within your IT club?",
+        answer: "Yes, absolutely! Our IT club offers various leadership opportunities for members who are interested in taking on roles such as event coordinators, workshop facilitators, project managers," +
+            " or executive board members. These positions provide valuable experience in leadership, teamwork, and project management.",
+    },
+    {
+        question: "How does your IT club engage with the broader tech community?",
+        answer: "Our IT club actively engages with the broader tech community through partnerships with local tech companies, participation in tech conferences and competitions, hosting guest speakers from the industry, and organizing networking events where members can connect with professionals in the field. yes"
+    },
+]
