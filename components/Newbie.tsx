d057466475d611f260bf602e5635beb44392efee
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-11 06:21:10 +0100

    Update Newbie.tsx
    Update Newbie.tsx

--- components/Newbie.tsx
+++ components/Newbie.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
-import Image from "next/image";
 import React from "react";
+import {FORM_LINK} from "@/data/constants";
 
 function Newbie() {
     return(
-        <div className="py-28">
+        <div className="py-28" id="newbie">
             <div className="bg-[#ED1C24] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 py-4 px-auto px-5 lg:px-40 lg:justify-center">
                 <div className="flex text-center uppercase text-white text-4xl py-10 lg:justify-start">
                     jois us, and become an active memeber in our future events
                 </div>
 
                <div className="flex items-center content-center justify-center px-auto pb-10 lg:pb-0 md:pb-0">
-                   <Link href="#"
+                   <Link href={FORM_LINK}
                          className="uppercase text-white text-center rounded-2xl p-2 bg-[#0d0d0d] lg:flex">
                        <div className="flex justify-center items-center py-1 pt-2 uppercase px-4 font-bold">
                            fill the form
