b5d5adbb278ed64c5309bd725fdbe43e718cdb30
Chihab Derghal <chihabderghal1@gmail.com>
2024-04-21 22:56:40 +0100

    Update Club.tsx

--- components/Club.tsx
+++ components/Club.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
+import {Roboto_Mono, Ubuntu_Mono} from "next/font/google";
+import {NextFont} from "next/dist/compiled/@next/font";
+
+const roboto: NextFont = Roboto_Mono({
+    weight: '500',
+    subsets: ['latin'],
+});
+
+const ubuntu: NextFont = Ubuntu_Mono({
+    weight: '700',
+    subsets: ['latin-ext'],
+});
 
 function Club() {
     return (
         <section
-            className="grid grid-cols-1 py-20 px-3 gap-8 md:grid-cols-1 lg:grid-cols-2 bg-[#ED1C24] my-28 text-white content-center lg:justify-around lg:pl-16"
+            className="grid grid-cols-1 py-10 px-3 gap-0 md:grid-cols-1 lg:grid-cols-2 bg-[#ED1C24] my-28 text-white content-center lg:justify-around lg:pl-16"
             id="about-us"
         >
-            <div className="flex flex-col justify-center text-center items-center uppercase py-5 lg:py-20">
-                <h1 className="text-4xl font-bold pb-10 text-center underline">About Our Club</h1>
-                <p className="font-bold text-2xl">
+            <div className="flex flex-col justify-center text-center  lg:mx-10 py-5 lg:py-20">
+                <h1 className={`text-4xl font-bold pb-10 text-center ${ubuntu.className}`}>About Our Club</h1>
+                <p className={`font-bold text-2xl text-center px-5 md:text-justify lg:text-justify ${roboto.className}`}>
                     Nextech, our scientific club located in NTIC Faculty, pushes boundaries ever further.
                     It is a dynamic ship propelling us into a realm where learning exceeds traditional constraints,
                     sailing through an ocean of creativity to collect knowledge in everything related to technology.
@@ -23,24 +35,10 @@ function Club() {
                 </p>
             </div>
 
-            <div className="grid grid-cols-1 lg:grid-cols-2">
-                <div className="flex flex-col items-center relative">
-                    <Image src="/university.png" alt="image" width="400" height="100"
-                           className=" border-2 border-white"/>
-                    <Image src="/university.png" alt="image" width="400" height="100"
-                           className=" border-2 border-white"/>
-                    <Image src="/university.png" alt="image" width="400" height="100"
-                           className=" border-2 border-white"/>
-                    <Image src="/university.png" alt="image" width="400" height="100"
-                           className=" border-2 border-white"/>
-                </div>
-                <div className="flex flex-col items-center">
-                    <Image src="/university.png" alt="image" width="400" height="100"
-                           className=" border-2 border-white"/>
-                    <Image src="/university.png" alt="image" width="400" height="100"
-                           className=" border-2 border-white"/>
-                    <Image src="/university.png" alt="image" width="400" height="100"
-                           className=" border-2 border-white"/>
+            <div className="grid grid-cols-1">
+                <div className="flex flex-col justify-center items-center relative">
+                    <Image src="/club.png" alt="image" width="600" height="800"
+                           className=" "/>
                 </div>
             </div>
         </section>
